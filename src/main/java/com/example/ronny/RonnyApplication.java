809473845f4eadba25c2fd6b6b403ca01c37166a
Hector Díaz <hdiaz@aubay.es>
2022-02-08 18:08:04 +0100

    Version inicial

---
+++ src/main/java/com/example/ronny/RonnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ronny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ronny/RonnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ronny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
